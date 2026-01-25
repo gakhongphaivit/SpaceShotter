9acd924a517c19f2794801d855513ca1bf6623fb
gakhongphaivit <vuga23052005@gmail.com>
2026-01-25 23:30:30 +0700

    DemoPart2

---
+++ Assets/Bullet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float flySpeed;
+
+    void Update()
+    {
+        var newPosition = transform.position;
+        newPosition.y += Time.deltaTime * flySpeed;
+        transform.position = newPosition;
+    }
+}
